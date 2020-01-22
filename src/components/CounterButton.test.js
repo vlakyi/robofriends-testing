63f2e05435d6251db7ecc480d95aecfcd53ba8d7
Vlad Kiyashko <kiyashko.vlad1@gmail.com>
2020-01-22 23:43:42 +0100

    adding tests

---
+++ src/components/CounterButton.test.js
@@ -0,0 +1,17 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import CounterButton from './CounterButton';
+
+it('expect to render CounterButton component', () => {
+    const mockColor = 'red';
+    expect(shallow(<CounterButton color={mockColor}/>)).toMatchSnapshot();
+});
+
+it('correctly increments the counter', () => {
+    const mockColor = 'red'
+    const wrapper = shallow(<CounterButton color={mockColor}/>);
+
+    wrapper.find('#counter').simulate('click');
+    expect(wrapper.state()).toEqual({count: 1});
+    expect(wrapper.props().color).toEqual('red')
+})
